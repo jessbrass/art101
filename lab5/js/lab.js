0260c42e2f8aa635569af6f62eedb6c2d0f4457f
jessbrass <jbrass@ucsc.edu>
2023-10-26 20:58:47 -0700

    lab 5 changes

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -6,6 +6,45 @@
 
 // Functions
 
+/**
+ * Author:    Jessie Brass
+ * Created:   10.24.2023
+ * License:   Public Domain
+ **/
+
+// Define variable
+make = "Mercedez Benz"
+model = "C300"
+color = "light blue"
+year = "2010"
+
+//calculate
+age = 2023 - year
+
+//output
+document.writeln ("make: " + make + "<br>");
+document.writeln ("model: " + model + "<br>");
+document.writeln ("color: " + color + "<br>");
+document.writeln ("year: " + year + "<br>");
+document.writeln ("age: " + age + " years<br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
