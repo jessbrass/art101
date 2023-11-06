1cd747e3cf9c94de7e4358d13a17987d5f2d4a58
jessbrass <jbrass@ucsc.edu>
2023-11-06 14:28:47 -0800

    lab 8

---
+++ lab8/js/ lab.js
@@ -0,0 +1,39 @@
+// index.js - Anon Functions and Callbacks
+// Author: Jessie
+// Date: Nov 5 2023
+// License: Public Domain
+
+// VARIABLES
+var myArray = [5, 10, 15, 20, 25, 30]
+
+// FUNCTIONS 
+function add5(x){
+    var results = (x + 5)
+    return results;
+}
+array.map(function(x){
+    var results = (x + 10)
+    return results;
+})
+ 
+//test function (with help of ChatGPT)
+console.log("The result of adding 5 and 5 is:", results);
+console.log("The result of adding 5 and 10 is:", results);
+// should return multiples of 5
+
+// erm?
+function numberone(test) {
+    console.log(test + "one")
+}
+
+function numbertwo(test) {
+    console.log(test + "two")
+}
+
+function numberthree(test) {
+    console.log(test + "three")
+    }
+
+numberone("Test 1: ");
+numberone("Test 2: ");
+numberone("Test 3: ");
